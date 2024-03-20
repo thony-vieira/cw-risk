5c96283b75e1503adf63e1e801a6c65bd1aee3b7
anthony vieira <thony16@gmail.com>
2024-03-20 00:10:13 -0300

    Feat: methods implementation. WIP, test phase.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
   has_many :cards
   has_many :transactions
+
+  # method to define what is the limit to transactions in the interval of an hour
+  def too_many_tx_in_row
+    max_consecutive_transactions = 3
+    time_zone = Time.find_zone('UTC')
+    one_hour_ago = time_zone - 1.hour
+    user_transactions = transactions.where(
+      "transaction_date >= ? AND transaction_date <= ?", one_hour_ago.beginning_of_hour, one_hour_ago.end_of_hour
+    ).order(transaction_date: :desc).limit(max_consecutive_transactions)
+    user_transactions.count < max_consecutive_transactions
+  end
+
+  # method to define if a transaction was made after certain hour and with a certain amount
+
+  def cbk_in_previous_transactions?
+    !transactions.exists?(has_cbk: true)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_18_224617) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_213150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,8 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_224617) do
 
   create_table "transactions", force: :cascade do |t|
     t.integer "merchant_id"
-    t.datetime "date", precision: nil
-    t.float "amount"
+    t.datetime "transaction_date", precision: nil
+    t.float "transaction_amount"
     t.bigint "user_id", null: false
     t.bigint "card_id", null: false
     t.datetime "created_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_224617) do
     t.integer "device_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   add_foreign_key "cards", "users"
