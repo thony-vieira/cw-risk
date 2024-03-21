e97bb6538460c00f903bb55cb3d578a0cd1ff57b
anthony vieira <thony16@gmail.com>
2024-03-20 21:41:51 -0300

    refact and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,22 @@ class User < ApplicationRecord
 
   # method to define what is the limit to transactions in the interval of an hour
   def too_many_tx_in_row(transaction)
-    max_consecutive_transactions = 3
+    max_consecutive_transactions = 4
     one_hour_ago = transaction.transaction_date - 1.hour
     user_transactions = transactions.where(
       "transaction_date >= ?", one_hour_ago
     ).order(transaction_date: :asc).limit(max_consecutive_transactions)
-    p user_transactions.count
     user_transactions.count < max_consecutive_transactions
   end
 
-  # method to define if a transaction was made after certain hour and with a certain amount
+  def too_many_tx_in_row_with_same_merchant(user_id, merchant_id, transaction)
+    max_consecutive_transactions = 3
+    one_hour_ago = transaction.transaction_date - 1.hour
+    user_transactions = transactions.where(
+      "user_id = ? AND merchant_id = ? AND transaction_date >= ?", user_id, merchant_id, one_hour_ago
+    ).order(transaction_date: :asc).limit(max_consecutive_transactions)
+    user_transactions.count < max_consecutive_transactions
+  end
 
   def cbk_in_previous_transactions?
     !transactions.exists?(has_cbk: true)
