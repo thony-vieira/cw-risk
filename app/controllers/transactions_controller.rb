754d6aea91922a041e4e8fdaf7c693d80ebc6ee5
anthony vieira <thony16@gmail.com>
2024-04-10 13:32:41 -0300

    Some fixes and infos

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,10 +5,13 @@ class TransactionsController < ApplicationController
     transaction = Transaction.new(transaction_params)
     transaction.user = User.find_or_initialize_by(user_params)
 
-    if transaction.anti_fraud_checked?
-      render json: { transaction_id: transaction.transaction_id, recomendation: 'approve' }
+    anti_fraud_result = transaction.anti_fraud_checked
+
+    if anti_fraud_result[:status]
+      render json: { transaction_id: transaction.transaction_id, recommendation: 'approve' }
     else
-      render json: { transaction_id: transaction.transaction_id, recomendation: 'deny' }
+      render json: { transaction_id: transaction.transaction_id, recommendation: 'deny',
+                     failed_checks: anti_fraud_result[:failed_check] }
     end
   end
 
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,9 +3,20 @@ class Transaction < ApplicationRecord
   belongs_to :card
 
   # Must return true to render approve
-  def anti_fraud_checked?
-    user.too_many_tx_in_row(self) && reject_tx_above_amount && user.cbk_in_previous_transactions? &&
-      user.too_many_tx_in_row_with_same_merchant(user_id, merchant_id, self)
+  def anti_fraud_checked
+    checks = {
+      'too_many_tx_in_row' => user.too_many_tx_in_row(self),
+      'reject_tx_above_amount' => reject_tx_above_amount,
+      'cbk_in_previous_transactions' => user.cbk_in_previous_transactions?,
+      'too_many_tx_in_row_with_same_merchant' => user.too_many_tx_in_row_with_same_merchant(user_id, merchant_id, self)
+    }
+
+    failed_checks = checks.select { |_key, value| value == false }
+    if failed_checks.any?
+      { status: false, failed_check: failed_checks.keys.join(", ") }
+    else
+      { status: true }
+    end
   end
 
   # method to define if a transaction was made after certain hour and with a certain amount
